b7e83edda4fc81a7de783310a854670880a73292
Artem Priadchenko <yorgan.info@gmail.com>
2023-07-04 23:02:36 +0300

    Добавьте файлы проекта.

---
+++ Task_2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+
+    public struct FileNames
+    {
+        public string NameFilIn { get; set; }
+        public string NameFilOu { get; set; }
+
+        public FileNames(string nameFilIn, string nameFilOu)
+        {
+            NameFilIn = nameFilIn;
+            NameFilOu = nameFilOu;
+        }
+
+    }
+
+
+    internal class Program
+    {
+
+        public static object ob = new object();
+
+        public static void CotiFile(object fileNames)
+        {
+            FileNames name = (FileNames)fileNames;
+
+            using StreamReader reader = new StreamReader(name.NameFilIn);
+            string temp = reader.ReadToEnd();
+            Console.WriteLine(temp);
+
+
+            //Monitor.Enter(ob);
+
+            using StreamWriter writer = File.AppendText(name.NameFilOu);
+            writer.WriteLine(temp);
+
+            //Monitor.Exit(ob);
+        }
+
+        static void Main()
+        {
+
+            FileNames fileNames1 = new FileNames("FilIn1.txt", "Res.txt");
+            FileNames fileNames2 = new FileNames("FilIn2.txt", "Res.txt");
+
+            Thread thread = new Thread(CotiFile);
+            Thread thread2 = new Thread(CotiFile);
+            thread.Start(fileNames1);
+
+            //thread.Join(); Если нужно прям вызов по порядкуж
+            thread2.Start(fileNames2);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static int counter = 0;
+        static object block = new object();
+
+        static void Function()
+        {
+            lock (block)
+            {
+                for (int i = 0; i < 10; ++i)
+                {
+                    Console.WriteLine(++counter);
+                }
+
+                Console.WriteLine(new String('-', 50));
+            }
+        }
+
+        static void Main()
+        {
+            Thread[] threads = { new Thread(Function), new Thread(Function), new Thread(Function) };
+
+            foreach (Thread thread in threads)
+                thread.Start();
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
